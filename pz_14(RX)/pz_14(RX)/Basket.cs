e4cb8414877a817706024baf39ed097786eb2d5b
Pyatin3pk1 <pyatin.aa1242@gmail.com>
2023-12-06 09:25:56 +0500

    first commit

---
+++ pz_14(RX)/pz_14(RX)/Basket.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pz_14_RX_
+{
+    class Basket
+    {
+        public string Name { get; set; }
+        public int Price { get; set; }
+        public int Quantity { get; set; }
+
+        public Basket()
+        {
+
+        }
+
+        public Basket(string name, int price , int quantity)
+        {
+            Name = name;
+            Price = price;
+            Quantity = quantity;
+        }
+    }
+}
---
+++ pz_14(RX)/pz_14(RX)/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace pz_14_RX_
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<Basket> baskets;
+
+        Basket currentBasket;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            baskets = new List<Basket>();
+            basketlIST.Items(baskets);
+        }
+
+        private void addBasketButton_Click(object sender, RoutedEventArgs e)
+        {
+            Basket bask = new Basket(Name.Text, int.Parse(Price.Text), int.Parse(Price.Text));
+            baskets.Add(bask);
+
+            ClearForm();
+        }
+
+        void ClearForm()
+        {
+            Name.Text = ""; Price.Text = ""; Quantity.Text = "";
+        }
+    }
+}
